79b91592ce24648cbc076141aacfdbc272df9469
kapsner <lorenz.kapsner@gmail.com>
2026-02-17 20:56:40 +0100

    chore: more git aliases

--- scripts/core/set_aliases.sh
+++ scripts/core/set_aliases.sh
@@ -20,6 +20,7 @@ alias gst='git status'
 alias ga='git add'
 alias gc='git commit'
 alias gd='git diff'
+alias gp='git push'
 
 # https://github.com/uker-troi-lab/commit_hooks
 alias bmp='pre-commit run --hook-stage post-commit bump-version'
