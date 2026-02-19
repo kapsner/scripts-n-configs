88e332c57a64f399c26583cfd8a4fc36210b1d58
kapsner <lorenz.kapsner@gmail.com>
2026-02-19 11:19:11 +0100

    chore: update set_aliases.sh

--- scripts/core/set_aliases.sh
+++ scripts/core/set_aliases.sh
@@ -21,6 +21,7 @@ alias ga='git add'
 alias gc='git commit'
 alias gd='git diff'
 alias gp='git push'
+alias gpua='git pull --all'
 
 # https://github.com/uker-troi-lab/commit_hooks
 alias bmp='pre-commit run --hook-stage post-commit bump-version'
